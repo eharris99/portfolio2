3bd173ffaa7e802dbb0bd71bb6f89c7b622c94de
Elise Harris <eharris99@yahoo.com>
2016-12-15 22:51:20 -0500

    link to

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "pages#welcome"
   get "/pages/portfolio" => "pages#portfolio"
   get "/pages/welcome" => "pages#welcome"
+  resources :pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
